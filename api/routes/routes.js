e22a34f9e51c00924d2abdd531086c2f58a0aa6f
RossEpsteinKY <31447738+RossEpsteinKY@users.noreply.github.com>
2022-06-23 23:10:40 -0400

    modified db and env:

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -3,6 +3,7 @@ const res = require("express/lib/response");
 const req = require("express/lib/request");
 const next = require("next");
 const router = express.Router();
+const postLogin = require('../../db/db');
 
 
 router.get("/", () =>{
@@ -13,7 +14,35 @@ router.get("/", () =>{
 });
 
 router.post("/login",(req,res,next)=>{
+    postLogin(req).then(result =>{
+       console.log(result);
+       res.status(200).json({
+           message: "Login saved",
+           status: 200,
+           login: {
+               userid: result.userid,
+               password: result.password,
+               metadata: {
+                   hostname: req.hostname,
+                   method: req.method
+               }
 
+           }
+       }).catch(err => {
+           res.status(500).json({
+               message: "Login failed",
+               status: 500,
+               error: {
+                   message: err.message,
+                   metadata: {
+                       hostname: req.hostname,
+                       method: req.method
+                   }
+
+               }
+           });
+       })
+    });
 });
 
 module.exports = router;
